b60d48f5de93ed7a3a00b56806b06ffc97e2dc59
ananyagupta1114 <ananyagupta1114@gmail.com>
2018-08-07 21:03:19 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -31,6 +31,15 @@ end
 
 def how_many_steps?
   # Write a loop that fulfills all criteria from Part 3 of the README.md
+  steps = 0
+  loop do
+    steps += 1
+    puts steps 
+    if steps%2 == 0
+      puts "Left"
+    else
+      puts "Right"
+    end
 end
 
 def break_dance
